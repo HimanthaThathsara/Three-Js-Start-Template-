493dc50c2f5757711c05c5f67a026e6396c6af72
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-04-15 00:04:12 +0530

    Create main.js

---
+++ Watch/main.js
@@ -0,0 +1,45 @@
+
+//  ========================================================================================  //
+
+// Initialize the viewer with the canvas element using the CoreViewerApp class and load the 3d model in the viewer. This is AutoCAD viewer.
+
+/* const viewer = new CoreViewerApp({canvas: document.getElementById('webgi-canvas')})
+viewer.initialize({
+    caching: true, 
+    ground: true,  //  Add ground to the scene with optional baked planar shadows, optional planar/screen-space reflections, background blending and more options
+    bloom: true,  //   Add bloom effect to the scene with optional settings like intensity, threshold, radius, etc. Also Adds post Bloom effect in HDR space , supports multiple bloom layers
+    depthTonemap: true,   //   Add depth tonemap effect to the scene with optional settings like exposure, gamma, etc. Also Adds post Depth Tonemap effect in HDR space
+    enableDrop: false,   //   Enables drag and drop for files and assets and links with the viewer. 
+    importPopup: false, //   Enables import popup for files and assets and links with the viewer.
+    debug: false       //   Enables debug mode for the viewer with optional settings like show stats, show grid, show axis, etc.
+}).then(()=>{
+    const model = "bmw_m3_need_for_speed_most_wanted.glb";  // 3d model path
+    viewer.load(model);  // Load the model in the viewer
+    console.log(viewer);
+}); */
+
+//  ========================================================================================  //
+
+
+
+// Initialize the viewer with the canvas element using the ViewerApp class and load the 3d model in the viewer. This is Manual viewer.
+
+
+
+
+
+// // The SDK can be added to package.json with module bundle and type declarations like this:
+// "devDependecies": {
+// 	"webgi": "https://dist.pixotronics.com/webgi/runtime/bundle-0.9.20.tgz",
+// 	"@types/webgi": "https://dist.pixotronics.com/webgi/runtime/bundle-types-0.9.20.tgz"
+// }
+
+// // The SDK can be imported in the main.js file like this:
+// import {ViewerApp} from "https://dist.pixotronics.com/webgi/runtime/bundle-0.9.20.mjs";  // Import the ViewerApp from link
+//import {ViewerApp} from "webgi"; // Import the ViewerApp from npm package
+
+
+// https://codepen.io/repalash/pen/mdpyrNa
+//You can see the imported JS files in CodePen by going into pen settings -> JS tab -> external files.
+
+
