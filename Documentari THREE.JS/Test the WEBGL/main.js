e1ccf5eb819639ac4132052a187ee0bba3a42b57
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-04-18 14:00:08 +0530

    Docs test

---
+++ Documentari THREE.JS/Test the WEBGL/main.js
@@ -0,0 +1,14 @@
+import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+
+if (WebGL.isWebGL2Available()) {
+
+	// Initiate function or other initializations here
+	animate();
+
+} else {
+
+	const warning = WebGL.getWebGL2ErrorMessage();
+	document.getElementById('container').appendChild(warning);
+
+}
