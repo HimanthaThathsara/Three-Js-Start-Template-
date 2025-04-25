539baec39053d992fcb6fa0df4d4cd323e63ccb5
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-04-25 22:05:53 +0530

    Create App.jsx

---
+++ 3D Product/Fisan pro/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./page/Home";
+import Navbar from "./compornent/Navbar";
+
+
+const App = () => {
+ 
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+};
+
+export default App;
